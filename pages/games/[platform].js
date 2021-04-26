121605d942ab1da04a1939cc09954c02842f0b74
ivan__emv12 <ivanmatosvillar@gmail.com>
2021-04-26 17:57:18 -0400

    Paginador creado

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -2,28 +2,44 @@ import React, { useState, useEffect } from 'react'
 import BasicLayout from '../../layouts/BasicLayout'
 import ListGames from '../../components/ListGames'
 import { useRouter } from 'next/router'
-import { startCase, camelCase } from "lodash"
-import { getGamesPlatformApi } from '../../api/games'
+import { getGamesPlatformApi, getTotalGamesPlatformApi } from '../../api/games'
 import { size } from 'lodash'
 import { Loader } from 'semantic-ui-react'
+import Pagination from '../../components/pagination'
 
-const limitPerPage = 10
+const limitPerPage = 20
 
 export default function Platform() {
 
     const router = useRouter()
-    const { query: { platform } } = router
+    const { query } = router
+    const { platform, page } = query
 
     const [games, setGames] = useState(null)
+    const [totalGames, setTotalGames] = useState(null)
+
+    const getStartItem = () => {
+        const currentPages = parseInt(page)
+        if (!page || currentPages === 1) return 0
+        else return currentPages * limitPerPage - limitPerPage
+    }
 
     useEffect(() => {
         (async () => {
             if (platform) {
-                const response = await getGamesPlatformApi(platform, limitPerPage, 0)
+                const response = await getGamesPlatformApi(platform, limitPerPage, getStartItem())
                 setGames(response)
             }
         })()
-    }, [platform])
+    }, [query])
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await getTotalGamesPlatformApi(platform)
+            setTotalGames(response)
+        })()
+    }, [query])
 
     return (
         <BasicLayout className="platform">
@@ -36,6 +52,14 @@ export default function Platform() {
             {games && size(games) > 0 && (
                 <ListGames games={games} />
             )}
+            {totalGames
+                ? (
+                    <Pagination
+                        totalGames={totalGames}
+                        page={page ? parseInt(page) : 1}
+                        limitPerPage={limitPerPage} />
+                )
+                : null}
         </BasicLayout>
     )
 }
